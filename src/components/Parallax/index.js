6345228cd18e7886c74d52014e197db4d00cc7da
David Roman <david_roman09@yahoo.com>
2021-04-02 15:43:28 -0500

    first attempt for parallex effect

---
+++ src/components/Parallax/index.js
@@ -0,0 +1,12 @@
+import { Parallax } from 'react-parallax';
+
+const ParallaxImage = () => (
+    <Parallax  bgImage= "" bgImageAlt="profile pic" strength={200} >
+        <div style={{height: 350}}>
+         
+        </div>
+
+    </Parallax>
+);
+
+export default ParallaxImage
